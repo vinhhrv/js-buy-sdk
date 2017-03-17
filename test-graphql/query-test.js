6190b39074a0b37315f74fc52c98dfaabdc98175
jzjiang <jessica.jiang@shopify.com>
2017-03-17 13:02:00 -0400

    add query testing for checkout

--- test-graphql/query-test.js
+++ test-graphql/query-test.js
@@ -8,6 +8,11 @@ import optionQuery from '../src-graphql/option-query';
 import imageQuery from '../src-graphql/image-query';
 import imageConnectionQuery from '../src-graphql/image-connection-query';
 import collectionQuery from '../src-graphql/collection-query';
+import checkoutQuery from '../src-graphql/checkout-query';
+import attributeQuery from '../src-graphql/attribute-query';
+import lineItemConnectionQuery from '../src-graphql/line-item-connection-query';
+import shippingRateQuery from '../src-graphql/shipping-rate-query';
+import mailingAddressQuery from '../src-graphql/mailing-address-query';
 
 suite('query-test', () => {
   const querySplitter = /[\s,]+/;
@@ -304,6 +309,115 @@ suite('query-test', () => {
                       }
                     }
                   }
+                }
+              }
+            }
+          }
+        }
+      }
+    }`;
+
+    assert.deepEqual(tokens(query.toString()), tokens(queryString));
+  });
+
+  test('it creates checkout queries (within a mutation) with default fields', () => {
+    const defaultQuery = checkoutQuery();
+    const query = client.graphQLClient.mutation((root) => {
+      root.add('checkoutCreate', (checkoutCreate) => {
+        defaultQuery(checkoutCreate, 'checkout');
+      });
+    });
+
+    const queryString = `mutation {
+      checkoutCreate {
+        checkout {
+          id
+          createdAt
+          updatedAt
+          requiresShipping
+          shippingLine {
+            handle
+            price
+            title
+          }
+          shippingAddress {
+            address1
+            address2
+            city
+            company
+            country
+            firstName
+            formatted
+            lastName
+            latitude
+            longitude
+            phone
+            province
+            zip
+            name
+            countryCode
+            provinceCode
+            id
+          }
+          lineItems (first: 250) {
+            pageInfo {
+              hasNextPage
+              hasPreviousPage
+            }
+            edges {
+              cursor
+              node {
+                title
+                variant {
+                  id
+                }
+                quantity
+                customAttributes {
+                  key
+                  value
+                }
+              }
+            }
+          }
+        }
+      }
+    }`;
+
+    assert.deepEqual(tokens(query.toString()), tokens(queryString));
+  });
+
+  test('it creates checkout queries (within a mutation) with specified fields', () => {
+    const customQuery = checkoutQuery(['id', 'createdAt', ['shippingLine', shippingRateQuery(['price'])],
+      ['shippingAddress', mailingAddressQuery(['address1'])],
+      ['lineItems', lineItemConnectionQuery(['title', ['customAttributes', attributeQuery(['value'])]])]]);
+    const query = client.graphQLClient.mutation((root) => {
+      root.add('checkoutCreate', (checkoutCreate) => {
+        customQuery(checkoutCreate, 'checkout');
+      });
+    });
+
+    const queryString = `mutation {
+      checkoutCreate {
+        checkout {
+          id
+          createdAt
+          shippingLine {
+            price
+          }
+          shippingAddress {
+            address1
+          }
+          lineItems (first: 250) {
+            pageInfo {
+              hasNextPage
+              hasPreviousPage
+            }
+            edges {
+              cursor
+              node {
+                title
+                customAttributes {
+                  value
                 }
               }
             }
